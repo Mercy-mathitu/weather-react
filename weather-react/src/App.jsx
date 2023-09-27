75afe1ad217f10e809454d31813e26e52e422c59
Mercy M. Wairimu <110403633+Mercy-mathitu@users.noreply.github.com>
2023-09-27 13:36:08 +0300

    Search added

---
+++ weather-react/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Search from "./Search";
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="App">
+      <h1>Weather Search Engine</h1>
+      <Search />
+    </div>
+    </>
+  )
+}
+
+export default App
